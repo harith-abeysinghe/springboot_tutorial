124a60830f03db5b36d4b6fb7265b88cf4a38ef1
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-06-09 14:09:03 +0530

    Initializing Maven

---
+++ tutorial/src/main/java/com/example/tutorial/TutorialApplication.java
@@ -0,0 +1,15 @@
+package com.example.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
