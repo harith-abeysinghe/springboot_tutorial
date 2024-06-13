5eae2ea5bbec0d95766350f9ec74358d35ab8891
harith-abeysinghe <e19003@eng.pdn.ac.lk>
2024-06-13 23:59:22 +0530

    Using ModelAndView

--- webapp/src/main/java/com/harith/webapp/HomeController.java
+++ webapp/src/main/java/com/harith/webapp/HomeController.java
@@ -2,7 +2,9 @@ package com.harith.webapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,14 +15,31 @@ public class HomeController {
     
 
     @RequestMapping("/home")
-    public String home(HttpServletRequest request) {
+    // public String home(HttpServletRequest request) {
         
-        HttpSession session = request.getSession(); // create a session
+    //     HttpSession session = request.getSession(); // create a session
         
-        String name = request.getParameter("name");
+    //     String name = request.getParameter("name");
 
-        System.out.println("Hello " + name);
-        session.setAttribute("name", name); //passing the name to the session
-        return "home";
+    //     System.out.println("Hello " + name);
+    //     session.setAttribute("name", name); //passing the name to the session
+    //     return "home";
+    // }
+    // public String home(@RequestParam("name") String myName, HttpSession session) { //instead of HttpServletRequest request
+
+    //     System.out.println("Hello " + myName);
+    //     session.setAttribute("name", myName); //passing the name to the session
+    //     return "home";
+    // }
+
+    //data - name
+    //view - home
+
+    //using ModelAndView
+    public ModelAndView home(@RequestParam("name") String myName) { //instead of HttpServletRequest request and session
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", myName);
+        mv.setViewName("home");
+        return mv;
     }
 }
